91f8e786a7f5378650378d7c5c2dd2cf8b4a627f
Yidong Ren <ryd994@163.com>
2016-10-31 16:51:43 -0400

    Use url_for

--- PackIt/event.py
+++ PackIt/event.py
@@ -5,3 +5,8 @@ from . import app
 @app.route("/event/new")
 def new_event():
     return render_template('newEvent.html')
+
+@app.route("/event/activity")
+def set_activity():
+    return render_template('selectActivity.html')
+    
