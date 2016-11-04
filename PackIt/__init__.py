eaf102610f91ed805e1c74dc1226a01527f07330
dan <dan@prs3.ryd994.net>
2016-11-04 04:21:36 +0100

    Refactor database model``

--- PackIt/__init__.py
+++ PackIt/__init__.py
@@ -3,11 +3,10 @@ from flask import Flask
 __all__ = ['app']
 
 app = Flask(__name__)
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config.from_envvar('CONFIG', silent=True) or app.config.from_pyfile('example.config')
 
- 
 from . import main
 from . import user
 from . import event
 from . import packing_list
+
--- PackIt/database.py
+++ PackIt/database.py
@@ -1,43 +1,90 @@
 from . import app
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.types import TypeDecorator, CHAR
+import uuid
 
 db = SQLAlchemy(app)
 
+class UUID(TypeDecorator):
+    impl = CHAR
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            if not isinstance(value, uuid.UUID):
+                return uuid.UUID(value).urn
+            else:
+                return value.urn
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            return uuid.UUID(value)
+
+
 class User(db.Model):
-    id = db.Column(db.String(100), primary_key=True)
+    __tablename__ = 'user'
+    id = db.Column(UUID, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    events = db.relationship('Event', back_populates='owner', uselist=True)
 
 
 class Event(db.Model):
-    id = db.Column(db.String(100), primary_key=True)
+    __tablename__ = 'event'
+    id = db.Column(UUID, primary_key=True)
     title = db.Column(db.String(100))
     destination = db.Column(db.String(100))
     eventtype = db.Integer()
-    user_id = db.relationship('User')
     begin = db.DateTime(timezone=True)
     end = db.DateTime(timezone=True)
     remind_at = db.DateTime(timezone=True)
     capacity = db.Integer()
+    owner_id = db.Column(UUID, db.ForeignKey('user.id'))
+    owner = db.relationship('User', back_populates='events', uselist=False)
+    activities = db.relationship('Activity', back_populates='event', uselist=True)
+    lists = db.relationship('ItemList', back_populates='event', uselist=True) 
 
 class Activity(db.Model):
-    id = db.Column(db.String(100), primary_key=True)
+    __tablename__ = 'activity'
+    id = db.Column(UUID, primary_key=True)
     title = db.Column(db.String(100))
-    event_id = db.relationship('Event', backref='activities')
+    event_id = db.Column(UUID, db.ForeignKey('event.id')) 
+    event = db.relationship('Event', back_populates='activities', uselist=False)
+    items = db.relationship('Item', back_populates='activity', uselist=True)
     
-class List(db.Model):
-    id = db.Column(db.String(100), primary_key=True)
+class ItemList(db.Model):
+    __tablename__ = 'item_list'
+    id = db.Column(UUID, primary_key=True)
     title = db.Column(db.String(100))
-    event_id = db.relationship('Event', backref='lists')
-    user_id = db.relationship('User')
+    event_id = db.Column(UUID, db.ForeignKey('event.id'))
+    event = db.relationship('Event', back_populates='lists', uselist=False)
+    owner_id = db.Column(UUID, db.ForeignKey('user.id'))
+    owner = db.relationship('User', uselist=False)
+    items = db.relationship('Item', back_populates='list', uselist=True)
     
 class Item(db.Model):
-    id = db.Column(db.String(100), primary_key=True)
+    __tablename__ = 'item'
+    id = db.Column(UUID, primary_key=True)
     title = db.Column(db.String(100))
-    user_id = db.relationship('User')
-    list_id = db.relationship('List', backref='items')
-    activity_id = db.relationship('Activity', backref='items')
+    owner_id = db.Column(UUID, db.ForeignKey('user.id'))
+    owner = db.relationship('User')
+    list_id = db.Column(UUID, db.ForeignKey('item_list.id'))
+    list = db.relationship('List', back_populates='items', uselist=False)
+    activity_id = db.Column(UUID, db.ForeignKey('activity.id'))
+    activity = db.relationship('Activity', back_populates='items', uselist=False)
+    checked_by = db.relationship("User", secondary='checked', uselist=True)
 
 class Checked(db.Model):
-    user_id = db.relationship('User')
-    item_id = db.relationship('Item')
+    __tablename__ = 'checked'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(UUID, db.ForeignKey('user.id'))
+    item_id = db.Column(UUID, db.ForeignKey('item.id'))
+
+
+if app.config['DROP_DATABASE']:
+    db.drop_all()
+if app.config['INIT_DATABASE']
+    db.create_all()
