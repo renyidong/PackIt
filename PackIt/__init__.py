eac36f145a678a259f26ad7184a7729dda11f8a4
Yidong Ren <c9@yidong.im>
2016-11-20 01:49:47 +0000

    Add freind

--- PackIt/__init__.py
+++ PackIt/__init__.py
@@ -8,6 +8,7 @@ app.config.from_envvar('CONFIG', silent=True) or app.config.from_pyfile('example
 
 from . import main
 from . import user
+from . import friend
 from . import event
 from . import packing_list
 
