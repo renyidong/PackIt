fde193de53d44fbdcc06b042b2b3226124c10c34
Yidong Ren <c9@yidong.im>
2016-11-20 02:41:52 +0000

    Integrate with user login for all pages. All pages should require login.

--- PackIt/user.py
+++ PackIt/user.py
@@ -1,20 +1,28 @@
 from flask import request, session, render_template, abort, redirect, flash, url_for
-import flask_login
+from flask_login import LoginManager, login_user, logout_user, login_required
+from urlparse import urlparse, urljoin
 
 from . import app
 from .database import db, User
 
 
-login_manager = flask_login.LoginManager(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and \
+           ref_url.netloc == test_url.netloc
+
+
 @app.route('/login', methods=['GET'])
 def login():
-    flash("Please Login")
     return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
@@ -25,34 +33,41 @@ def do_login():
     user = User.query.filter_by(username=username).first()
     
     if user and user.password == password:
-        flask_login.login_user(user)
+        login_user(user)
         flash('You are logged in.')
         next = request.args.get('next')
-        return redirect(next or url_for('home'))
+        if next and is_safe_url(next):
+            return redirect(next)
+        else:
+            return redirect(url_for('home'))
+            
     else:
         flash("Wrong username or password.")
         return redirect(url_for('login'))
 
 
 @app.route('/logout')
-@flask_login.login_required  
+@login_required
 def logout():
-    flask_login.logout_user()
+    logout_user()
     flash('You are logged out.')
     next = request.args.get('next')
     return redirect(next or url_for('home'))
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET'])
 def signup():
     return render_template('signup.html')
     
 @app.route('/signup', methods=['POST'])
 def do_signup():
-    username = request.args['username']
-    email = request.args['email']
-    password = request.args['password']
+    username = request.form['username']
+    email = request.form['email']
+    password = request.form['password']
     
     user = User.new(username, email, password)
     db.session.add(user)
+    db.session.commit()
+
     flash('You are signed up!')
+    redirect(url_for('login'))
     return redirect(url_for('login'))
