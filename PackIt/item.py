63947b70af76f558d7ea71537ff2aa87526bcefe
Yidong Ren <c9@yidong.im>
2016-11-21 06:04:07 +0000

    BugFix

--- PackIt/item.py
+++ PackIt/item.py
@@ -63,7 +63,7 @@ def item_del(item_id):
     if i.owner_id != current_user.id:
         raise Forbidden
     
-    db.session.delete(delete)
+    db.session.delete(i)
     db.session.commit()
     return 'OK'
 
@@ -73,19 +73,19 @@ def item_del(item_id):
 def item_new_uni():
     """Create item given a browser form."""
     item_new()
-    return redirect(url_for('packing_list', list_id=request.form['list_id']))
+    return redirect(request.header['Referrer'])
     
 @app.route("/item/put", methods=["POST"])
 @login_required
 def item_put_uni():
     """Update item given a browser form."""
     item_put(item_id = request.form['id'])
-    return redirect(url_for('packing_list', list_id=request.form['list_id']))
+    return redirect(request.header['Referrer'])
     
 @app.route("/item/del", methods=["POST"])
 @login_required
 def item_del_uni():
     """Delete item given a browser form."""
     item_del(item_id = request.form['id'])
-    return redirect(url_for('packing_list', list_id=request.form['list_id']))
+    return redirect(request.header['Referrer'])
     
